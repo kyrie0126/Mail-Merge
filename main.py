962111270b67279f1f2d6aaae4195cbb51cb7250
kyrie0126 <kylewong00@gmail.com>
2022-11-07 23:49:01 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-#TODO: Create a letter using starting_letter.txt
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
 with open("C:/Users/Kyrie/Documents/100 Days of Code/Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter_text:
     letter = letter_text.read()
 
